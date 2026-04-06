eb6f3ca598c7b7ff56f5e0d85b42f2b339219d22
Markmhbr <markmhbr@example.com>
2026-04-06 22:49:55 +0700

    Perbaikan

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -32,55 +32,6 @@ class UserSeeder extends Seeder
             ]
         );
 
-        // 3. Create Jabatan
-        $jabatanKasir = Jabatan::updateOrCreate(
-            ['nama_jabatan' => 'Kasir'],
-            ['deskripsi' => 'Pengelola transaksi dan keuangan di outlet.']
-        );
-
-        $jabatanDriver = Jabatan::updateOrCreate(
-            ['nama_jabatan' => 'Driver'],
-            ['deskripsi' => 'Petugas layanan antar-jemput cucian pelanggan.']
-        );
-
-        // 4. Create Karyawan - KASIR
-        $userKasir = User::updateOrCreate(
-            ['email' => 'karyawan@gmail.com'],
-            [
-                'name' => 'Karyawan Kasir',
-                'password' => Hash::make('12345678'),
-                'role' => 'karyawan',
-            ]
-        );
-
-        Karyawan::updateOrCreate(
-            ['user_id' => $userKasir->id],
-            [
-                'jabatan_id' => $jabatanKasir->id,
-                'tanggal_masuk' => now(),
-                'status_kerja' => 'aktif',
-                'no_hp' => '08123456789',
-            ]
-        );
-
-        // 5. Create Karyawan - DRIVER
-        $userDriver = User::updateOrCreate(
-            ['email' => 'driver@gmail.com'],
-            [
-                'name' => 'Karyawan Driver',
-                'password' => Hash::make('12345678'),
-                'role' => 'karyawan',
-            ]
-        );
-
-        Karyawan::updateOrCreate(
-            ['user_id' => $userDriver->id],
-            [
-                'jabatan_id' => $jabatanDriver->id,
-                'tanggal_masuk' => now(),
-                'status_kerja' => 'aktif',
-                'no_hp' => '08987654321',
-            ]
-        );
+        
     }
 }
